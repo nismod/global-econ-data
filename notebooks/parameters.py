238313996ff45ad3dfebfabb23f2c2c57d613977
ischlo <ivann.schlosser.19@ucl.ac.uk>
2024-09-10 10:18:09 +0100

    notebook with geometries geometry for dose-wdi

---
+++ notebooks/parameters.py
@@ -0,0 +1,7 @@
+# paramers.py
+
+# this is the year for which to generate the dose-wdi data set, easier to set from here than looking for it across the notebook.
+year = 2015
+
+# 
+missing_frac = 0.5
